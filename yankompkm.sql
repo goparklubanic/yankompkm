8dced8dc81ddd857c2cb219b0aee9b16063ee3aa
goparklubanic <ngoparios@yahoo.com>
2017-09-25 06:15:59 +0700

    db dump

---
+++ yankompkm.sql
@@ -0,0 +1,333 @@
+-- MySQL dump 10.13  Distrib 5.5.55, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: sms
+-- ------------------------------------------------------
+-- Server version	5.5.55-0+deb8u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `gammu`
+--
+
+DROP TABLE IF EXISTS `gammu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gammu` (
+  `Version` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Version`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gammu`
+--
+
+LOCK TABLES `gammu` WRITE;
+/*!40000 ALTER TABLE `gammu` DISABLE KEYS */;
+INSERT INTO `gammu` VALUES (16);
+/*!40000 ALTER TABLE `gammu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inbox`
+--
+
+DROP TABLE IF EXISTS `inbox`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inbox` (
+  `UpdatedInDB` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ReceivingDateTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `Text` text NOT NULL,
+  `SenderNumber` varchar(20) NOT NULL DEFAULT '',
+  `Coding` enum('Default_No_Compression','Unicode_No_Compression','8bit','Default_Compression','Unicode_Compression') NOT NULL DEFAULT 'Default_No_Compression',
+  `UDH` text NOT NULL,
+  `SMSCNumber` varchar(20) NOT NULL DEFAULT '',
+  `Class` int(11) NOT NULL DEFAULT '-1',
+  `TextDecoded` varchar(160) DEFAULT NULL,
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `RecipientID` text NOT NULL,
+  `Processed` enum('false','true') NOT NULL DEFAULT 'false',
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inbox`
+--
+
+LOCK TABLES `inbox` WRITE;
+/*!40000 ALTER TABLE `inbox` DISABLE KEYS */;
+INSERT INTO `inbox` VALUES ('2017-08-30 02:42:34','2017-04-04 02:41:15','00500061006B0020006E007500670020006C006700200064006D006E003F00200053007900200064006100680020006400690064006500700061006E002000720075006D0061006800650020006A0065006E0065006E00670061006E002E','+6285329400632','Default_No_Compression','','+6281100000',-1,'Pak nug lg dmn? Sy dah didepan rumahe jenengan.',1,'','false'),('2017-08-30 02:42:34','2017-04-04 02:53:02','00500061006B0020006E007500670072006F0068006F002C0020006E0061006E0074006900200070006C0067006E007900610020006D0061006D007000690072002000700069006B006F006D0020006E0067006700690068002E000A004E00770075006E0020000A004C004B0050002000500049004B004F004D0020000A00420061006E006A00610072006E00650067006100720061','+6285227098009','Default_No_Compression','','+6281100000',-1,'Pak nugroho, nanti plgnya mampir pikom nggih.\nNwun \nLKP PIKOM \nBanjarnegara',2,'','false'),('2017-08-30 02:42:34','2017-04-08 12:56:54','00610079006900730020007700690073002000740065006B0061006E00200061007000610020006E006A0065006E0065006E00670061006E00200061006E00750020007500720075006E0067002000720061006D00700075006E0067003F','+6285245247525','Default_No_Compression','','+6281100000',-1,'ayis wis tekan apa njenengan anu urung rampung?',5,'','false'),('2017-08-30 02:42:34','2017-06-06 23:43:56','00500061006B0020006E00750067002000700075006E00790061002000620075006B0075003200200079006700200074006E007400670020006B0065006D006100720065006E0020006100640061002000640069002000620061006200200032003F003F0020','+6285643141343','Default_No_Compression','','+62816124',-1,'Pak nug punya buku2 yg tntg kemaren ada di bab 2?? ',7,'','false'),('2017-08-30 02:42:34','2017-06-07 01:27:20','00500061006B00200069006E0069002000730061007900610020007400650078007400200065006400690074006F0072006E00790061002000700061006B00650020006100700061003F002000410074006F006D00200061007400610075002000610070006100610061003F','+6285643141343','Default_No_Compression','','+62816124',-1,'Pak ini saya text editornya pake apa? Atom atau apaaa?',8,'','false'),('2017-08-30 04:01:13','2017-08-30 04:00:04','006B006F006E002000730065007400740069006E00670020006B006F006D00700075007400650072002000730065006B006F006C006100680061006E0020004D004100430020006D0061006E0069006E0067003F0020','+6281327284182','Default_No_Compression','','+62811078801',-1,'kon setting komputer sekolahan MAC maning? ',10,'','false');
+/*!40000 ALTER TABLE `inbox` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`berita`@`localhost`*/ /*!50003 TRIGGER inbox_timestamp BEFORE INSERT ON inbox
+FOR EACH ROW
+BEGIN
+    IF NEW.ReceivingDateTime = '0000-00-00 00:00:00' THEN
+        SET NEW.ReceivingDateTime = CURRENT_TIMESTAMP();
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `outbox`
+--
+
+DROP TABLE IF EXISTS `outbox`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `outbox` (
+  `UpdatedInDB` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `InsertIntoDB` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `SendingDateTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `SendBefore` time NOT NULL DEFAULT '23:59:59',
+  `SendAfter` time NOT NULL DEFAULT '00:00:00',
+  `Text` text,
+  `DestinationNumber` varchar(20) NOT NULL DEFAULT '',
+  `Coding` enum('Default_No_Compression','Unicode_No_Compression','8bit','Default_Compression','Unicode_Compression') NOT NULL DEFAULT 'Default_No_Compression',
+  `UDH` text,
+  `Class` int(11) DEFAULT '-1',
+  `TextDecoded` text NOT NULL,
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `MultiPart` enum('false','true') DEFAULT 'false',
+  `RelativeValidity` int(11) DEFAULT '-1',
+  `SenderID` varchar(255) DEFAULT NULL,
+  `SendingTimeOut` timestamp NULL DEFAULT '0000-00-00 00:00:00',
+  `DeliveryReport` enum('default','yes','no') DEFAULT 'default',
+  `CreatorID` text NOT NULL,
+  `Retries` int(3) DEFAULT '0',
+  `Priority` int(11) DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `outbox_date` (`SendingDateTime`,`SendingTimeOut`),
+  KEY `outbox_sender` (`SenderID`(250))
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `outbox`
+--
+
+LOCK TABLES `outbox` WRITE;
+/*!40000 ALTER TABLE `outbox` DISABLE KEYS */;
+/*!40000 ALTER TABLE `outbox` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`berita`@`localhost`*/ /*!50003 TRIGGER outbox_timestamp BEFORE INSERT ON outbox
+FOR EACH ROW
+BEGIN
+    IF NEW.InsertIntoDB = '0000-00-00 00:00:00' THEN
+        SET NEW.InsertIntoDB = CURRENT_TIMESTAMP();
+    END IF;
+    IF NEW.SendingDateTime = '0000-00-00 00:00:00' THEN
+        SET NEW.SendingDateTime = CURRENT_TIMESTAMP();
+    END IF;
+    IF NEW.SendingTimeOut = '0000-00-00 00:00:00' THEN
+        SET NEW.SendingTimeOut = CURRENT_TIMESTAMP();
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `outbox_multipart`
+--
+
+DROP TABLE IF EXISTS `outbox_multipart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `outbox_multipart` (
+  `Text` text,
+  `Coding` enum('Default_No_Compression','Unicode_No_Compression','8bit','Default_Compression','Unicode_Compression') NOT NULL DEFAULT 'Default_No_Compression',
+  `UDH` text,
+  `Class` int(11) DEFAULT '-1',
+  `TextDecoded` text,
+  `ID` int(10) unsigned NOT NULL DEFAULT '0',
+  `SequencePosition` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`ID`,`SequencePosition`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `outbox_multipart`
+--
+
+LOCK TABLES `outbox_multipart` WRITE;
+/*!40000 ALTER TABLE `outbox_multipart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `outbox_multipart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `phones`
+--
+
+DROP TABLE IF EXISTS `phones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `phones` (
+  `ID` text NOT NULL,
+  `UpdatedInDB` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `InsertIntoDB` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `TimeOut` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `Send` enum('yes','no') NOT NULL DEFAULT 'no',
+  `Receive` enum('yes','no') NOT NULL DEFAULT 'no',
+  `IMEI` varchar(35) NOT NULL,
+  `IMSI` varchar(35) NOT NULL,
+  `NetCode` varchar(10) DEFAULT 'ERROR',
+  `NetName` varchar(35) DEFAULT 'ERROR',
+  `Client` text NOT NULL,
+  `Battery` int(11) NOT NULL DEFAULT '-1',
+  `Signal` int(11) NOT NULL DEFAULT '-1',
+  `Sent` int(11) NOT NULL DEFAULT '0',
+  `Received` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`IMEI`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `phones`
+--
+
+LOCK TABLES `phones` WRITE;
+/*!40000 ALTER TABLE `phones` DISABLE KEYS */;
+INSERT INTO `phones` VALUES ('','2017-08-30 04:24:12','2017-08-30 03:52:12','2017-08-30 04:24:22','yes','yes','867597001675294','510017932301716','510 01','INDOSATOOREDOO','Gammu 1.38.4, Linux, kernel 3.16.0-4-amd64 (#1 SMP Debian 3.16.43-2+deb8u2 (2017-06-26)), GCC 4.9',0,51,1,1);
+/*!40000 ALTER TABLE `phones` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`berita`@`localhost`*/ /*!50003 TRIGGER phones_timestamp BEFORE INSERT ON phones
+FOR EACH ROW
+BEGIN
+    IF NEW.InsertIntoDB = '0000-00-00 00:00:00' THEN
+        SET NEW.InsertIntoDB = CURRENT_TIMESTAMP();
+    END IF;
+    IF NEW.TimeOut = '0000-00-00 00:00:00' THEN
+        SET NEW.TimeOut = CURRENT_TIMESTAMP();
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `sentitems`
+--
+
+DROP TABLE IF EXISTS `sentitems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sentitems` (
+  `UpdatedInDB` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `InsertIntoDB` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `SendingDateTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `DeliveryDateTime` timestamp NULL DEFAULT NULL,
+  `Text` text NOT NULL,
+  `DestinationNumber` varchar(20) NOT NULL DEFAULT '',
+  `Coding` enum('Default_No_Compression','Unicode_No_Compression','8bit','Default_Compression','Unicode_Compression') NOT NULL DEFAULT 'Default_No_Compression',
+  `UDH` text NOT NULL,
+  `SMSCNumber` varchar(20) NOT NULL DEFAULT '',
+  `Class` int(11) NOT NULL DEFAULT '-1',
+  `TextDecoded` varchar(160) DEFAULT NULL,
+  `ID` int(10) unsigned NOT NULL DEFAULT '0',
+  `SenderID` varchar(255) NOT NULL,
+  `SequencePosition` int(11) NOT NULL DEFAULT '1',
+  `Status` enum('SendingOK','SendingOKNoReport','SendingError','DeliveryOK','DeliveryFailed','DeliveryPending','DeliveryUnknown','Error') NOT NULL DEFAULT 'SendingOK',
+  `StatusError` int(11) NOT NULL DEFAULT '-1',
+  `TPMR` int(11) NOT NULL DEFAULT '-1',
+  `RelativeValidity` int(11) NOT NULL DEFAULT '-1',
+  `CreatorID` text NOT NULL,
+  PRIMARY KEY (`ID`,`SequencePosition`),
+  KEY `sentitems_date` (`DeliveryDateTime`),
+  KEY `sentitems_tpmr` (`TPMR`),
+  KEY `sentitems_dest` (`DestinationNumber`),
+  KEY `sentitems_sender` (`SenderID`(250))
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sentitems`
+--
+
+LOCK TABLES `sentitems` WRITE;
+/*!40000 ALTER TABLE `sentitems` DISABLE KEYS */;
+INSERT INTO `sentitems` VALUES ('2017-08-30 03:37:00','2017-08-30 03:34:08','2017-08-30 03:37:00',NULL,'00540065007300200053006D007300200056006900610020004C006100700074006F0070','085729876074','Default_No_Compression','','+62816124',-1,'Tes Sms Via Laptop',3,'',1,'SendingOKNoReport',-1,113,255,'Nugroho'),('2017-08-30 03:37:33','2017-08-30 03:37:22','2017-08-30 03:37:33',NULL,'00540065007300200053006D007300200056006900610020004C006100700074006F0070','085729876074','Default_No_Compression','','+62816124',-1,'Tes Sms Via Laptop',4,'',1,'SendingOKNoReport',-1,114,255,'Nugroho'),('2017-08-30 04:23:15','2017-08-30 04:22:44','2017-08-30 04:23:15',NULL,'0053006900610070002E0020004B006100700061006E0020006A0061006D00200070006900720061003F','081327284182','Default_No_Compression','','+62816124',-1,'Siap. Kapan jam pira?',5,'',1,'SendingOKNoReport',-1,115,255,'Nugroho');
+/*!40000 ALTER TABLE `sentitems` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`berita`@`localhost`*/ /*!50003 TRIGGER sentitems_timestamp BEFORE INSERT ON sentitems
+FOR EACH ROW
+BEGIN
+    IF NEW.InsertIntoDB = '0000-00-00 00:00:00' THEN
+        SET NEW.InsertIntoDB = CURRENT_TIMESTAMP();
+    END IF;
+    IF NEW.SendingDateTime = '0000-00-00 00:00:00' THEN
+        SET NEW.SendingDateTime = CURRENT_TIMESTAMP();
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-09-25  6:15:44
